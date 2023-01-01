3acfde289198950be1149cb1fad6b69569f6c6eb
Vincentius P41siseus <vincentiusiseus@gmail.com>
2023-01-02 08:28:14 +0900

    Copy paste basic code & add new scrape dirs

---
+++ src/dnf-api/index.ts
@@ -0,0 +1,73 @@
+// Node libs
+import * as https from "https"
+import * as crypto from 'crypto'
+import * as fs from "fs"
+
+// NPM libs
+import _axios from 'axios'
+
+const axios = _axios.create({
+    httpsAgent: new https.Agent({
+        keepAlive: true,
+        /**
+         * 2022-12-31 16:16
+         * Prevent `SSL routines:final_renegotiate:unsafe legacy renegotiation disabled`
+         * */
+        secureOptions: crypto.constants.SSL_OP_LEGACY_SERVER_CONNECT,
+    })
+})
+
+class DnfApi {
+    api_key:string
+
+    static BASE_URL = `https://api.neople.co.kr/df/`
+
+    constructor(api_key:string) {
+        this.api_key = api_key
+    }
+
+    _makeUrl(path:string) {
+        const url = new URL(path, DnfApi.BASE_URL)
+        url.searchParams.set("apikey", this.api_key)
+        return url
+    }
+    
+    /**
+     * 2022-12-31 17:08
+     * Fixed output.
+     */
+    async getServerIds() {
+        const url = this._makeUrl("./servers")
+        const response = await axios.get(url.href)
+        return response.data
+    }
+
+    async getCharacter(server_id:string, name:string) {
+        const url = this._makeUrl(`./servers/${server_id}/characters`)
+        url.searchParams.append("characterName", name)
+        const response = await axios.get(url.href)
+        return response.data
+    }
+
+    async getCharacterInfo(server_id:string, id:string) {
+        const url = this._makeUrl(`./servers/${server_id}/characters/${id}`)
+        const response = await axios.get(url.href)
+        return response.data
+    }
+}
+
+async function main() {
+    const api_key = fs.readFileSync("./cred/api.txt", "utf-8").trim()
+    const inst = new DnfApi(api_key)
+    let data:any = null
+    try {
+        data = await inst.getCharacter("all", "이창에흑염룡")
+    }
+    catch(e) {
+        console.log(e.response.data)
+        console.log(e.response)
+        // throw e
+    }
+    console.log(data)
+}
+main()
---
+++ src/dnf-forum-scrape/index.ts
@@ -0,0 +1,123 @@
+// NPM libs
+import { JSDOM } from "jsdom"
+import _axios, { AxiosResponse } from "axios"
+import iconv from "iconv-lite"
+
+// NPM Types
+import type { Db } from "mongodb"
+
+// Node libs
+import * as fs from "fs"
+import * as crypto from "crypto"
+import * as https from "https"
+
+// My libs
+import { client } from "~/src/db"
+
+const axios = _axios.create({
+    httpsAgent: new https.Agent({
+        keepAlive: true,
+        /**
+         * 2022-12-31 16:16
+         * Prevent `SSL routines:final_renegotiate:unsafe legacy renegotiation disabled`
+         * */
+        secureOptions: crypto.constants.SSL_OP_LEGACY_SERVER_CONNECT,
+    })
+})
+
+class Main {
+    static BASE_URL = `https://df.nexon.com/df/community/dnfboard?mode=list&order=reg_date&order_type=DESC`
+
+    db:Db
+    page:number
+    stop_flag:boolean
+    
+    constructor() {
+        this.page = 0
+        // this.page = 33823
+        this.stop_flag = false
+    }
+
+    async loadPage() {
+        const url = new URL("", Main.BASE_URL)
+        url.searchParams.append("page", "" + this.page)
+        const response = await axios.get(url.href, { responseType: "arraybuffer" })
+        const html_content = response.data
+        return html_content
+    }
+
+    async scrapePosts() {
+        const html_content:Buffer = await this.loadPage()
+        const decoded_html_content = iconv.decode(html_content, "euckr")
+        const jsdom = new JSDOM(decoded_html_content)
+        const document = jsdom.window.document
+        const post_row_elements = document.querySelectorAll("table.tbl tr:not(.ntc)")
+        let post_row_array = Array.from(post_row_elements)
+        post_row_array = post_row_array.filter(row => row.querySelector("th") == null && row.querySelector("td.author") != null)
+
+        if(post_row_array.length == 0) {
+            this.stop_flag = true
+            console.log(`Stop flag at page ${this.page}`)
+            return
+        }
+
+        const output = []
+        for(const post_row of post_row_array) {
+            // if(post_row.querySelector("th") != null) continue;
+            const post_link_el = post_row.querySelector(".tit a")
+            /**
+             * 2023-01-01 14:05
+             * "신고에 의해 숨김 처리된 게시물입니다" has no `a` element under `td.tit`
+             */
+            if(post_link_el == null) continue 
+            const post_link = post_link_el.getAttribute("href")
+            const post_sp = new URLSearchParams(post_link)
+            const post_id = post_sp.get("no")
+            const target_el = post_row.querySelector(".author span")
+            /**
+             * 2023-01-01 11:58
+             * data-key에 유저 id, data-sv에 1-indexed 서버 index 있지만 최대한 많은 유저를 찾는것이 목표 이기 때문에
+             * 스킵
+             */
+            const user_name = target_el.textContent.trim()
+            const date = post_row.querySelector("td:nth-child(3)").textContent.trim()
+
+            output.push({ user_name, post_id, date, post_link })
+        }
+        return output
+    }
+
+    async start() {
+        const start_dt = new Date()
+
+        console.log("Start")
+        await client.connect()
+        this.db = client.db("dnf-data")
+
+        while(true) {
+            console.log(`Load page ${this.page}`)
+            await this.loadPage()
+            const output = await this.scrapePosts()
+            if(output != null && output.length > 0) {
+                console.log(`Insert ${output.length} entries.`)
+                await this.db.collection("forum-users").insertMany(output)
+            }
+            console.log(`Done for page ${this.page}. Took ${new Date().getTime() - start_dt.getTime()}ms so far.`)
+
+            if(this.stop_flag) break;
+
+            this.page++
+        } 
+        console.log(`Done`)
+        await client.close()
+        // fs.writeFileSync("./data/html_content.html", html_content)
+
+        const end_dt = new Date()
+        console.log(`Took ${end_dt.getTime() - start_dt.getTime()}ms`)
+    }
+}
+
+async function main() {
+    await new Main().start()
+}
+main()
