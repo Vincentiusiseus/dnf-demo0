baf3255d4b91be17ddb1bdde8271e3733a95e1da
Vincentius P41siseus <vincentiusiseus@gmail.com>
2023-01-02 12:38:22 +0900

    Refactor & fix bug
    이전 WorkerManager를 사용해서 IncValue 재구현. 테스트 목적도 있음.
    
    버그는 random_wait_ms 대신 2000라는 상수가 사용되고 있어서 Main thread와
    worker thread가 random setTimeout 변수에도 불구하고 '사이좋게' 통신했음.

--- src/worker-thread/index.ts
+++ src/worker-thread/index.ts
@@ -11,27 +11,52 @@ class WorkerManager {
         this.value = 0
     }
 
-    startWorker(worker_script_path:string) {
+    async prepareArgs(thread_id:number):Promise<any> {}
+
+    async handleResponse(thread_id:number, response:any) {}
+
+    async postMessage(worker:Worker) {
+        const args = await this.prepareArgs(worker.threadId)
+        worker.postMessage(args)
+    }
+
+    async startWorker(worker_script_path:string) {
         const worker = new Worker(worker_script_path)
         this.workers[worker.threadId] = worker
 
-        const min = 500
-        const max = 3000
-        const random_wait_ms = Math.random() * (max - min) + min;
-        console.log(`[${new Date().toISOString()}][Main] Send message to ${worker.threadId} after ${random_wait_ms}`)
-        setTimeout(() => {
-            worker.postMessage(this.value++)
-        }, random_wait_ms)
-
-        worker.on("message", (msg) => {
-            console.log(`[${new Date().toISOString()}][Main] Received msg ${msg} from ${worker.threadId}`)
-            worker.postMessage(this.value++)
+        worker.on("message", async (response) => {
+            await this.handleResponse(worker.threadId, response)
+            await this.postMessage(worker)
         })
+
+        await this.postMessage(worker)
+    }
+}
+
+class IncValue extends WorkerManager {
+    async prepareArgs(thread_id:number): Promise<number> {
+        const value = this.value
+        const min = 500
+        const max = 5000
+        const random_wait_ms = Math.random() * (max - min) + min;
+        const expected_time = new Date(new Date().getTime() + random_wait_ms).toISOString()
+        this.value++
+        console.log(`[${new Date().toISOString()}][Main][${thread_id}] Value ${value} with setTimeout ${random_wait_ms} (expected time ${expected_time})`)
+        await new Promise((res,rej) => {
+            setTimeout(() => {
+               res(0) 
+            }, random_wait_ms);
+        })
+        return value
+    }
+    
+    async handleResponse(thread_id:number, response: any): Promise<void> {
+        console.log(`[${new Date().toISOString()}][Main][${thread_id}] Received response`)
     }
 }
 
 function main() {
-    const worker_manager = new WorkerManager()
+    const worker_manager = new IncValue()
     worker_manager.startWorker(path.join(__dirname, "./worker.js"))
     worker_manager.startWorker(path.join(__dirname, "./worker.js"))
     worker_manager.startWorker(path.join(__dirname, "./worker.js"))
--- src/worker-thread/worker.ts
+++ src/worker-thread/worker.ts
@@ -4,8 +4,9 @@ parentPort.on("message", (value) => {
     const min = 500
     const max = 3000
     const random_wait_ms = Math.random() * (max - min) + min;
-    console.log(`[${new Date().toISOString()}][worker ${threadId}] Recevied message ${value}. Send back after ${random_wait_ms}`)
+    const expected_time = new Date(new Date().getTime() + random_wait_ms).toISOString()
+    console.log(`[${new Date().toISOString()}][worker ${threadId}] Recevied message ${value}. Send back after ${random_wait_ms} (expected time ${expected_time})`)
     setTimeout(() => {
         parentPort.postMessage(`Handled ${value}`)
-    }, 2000)
+    }, random_wait_ms)
 })
