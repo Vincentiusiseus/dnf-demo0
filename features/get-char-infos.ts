2fb5275d8734fa005112b28f330719664f62dcfb
Vincentius P41siseus <vincentiusiseus@gmail.com>
2023-01-09 17:21:46 +0900

    Add new file

---
+++ features/get-char-infos.ts
@@ -0,0 +1,20 @@
+// NPM types
+import type { Db, MongoClient } from "mongodb"
+
+// My libs
+import { client } from "~/src/db"
+
+class Main {
+    constructor() {
+
+    }
+
+    async start() {
+
+    }
+}
+
+async function main() {
+
+}
+main()
