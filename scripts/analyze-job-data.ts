1c32ab12ac4bed986bc513a8a31c0ad590cc4103
Vincentius P41siseus <vincentiusiseus@gmail.com>
2023-01-14 17:04:29 +0900

    Add new script
    여기서 못벗어나고 있긴 하지만 해결하지 않으면 이것때문에 막힐것 같기에
    어쩔수 없이...

---
+++ scripts/analyze-job-data.ts
@@ -0,0 +1,94 @@
+// Node libs
+import * as fs from "fs"
+
+class Main {
+    char_infos:any[]
+    count_advs:any
+    count_awks:any
+
+    constructor() {
+
+    }
+
+    *genAdvs() {
+        for(const char_info of this.char_infos) {
+            const char_name = char_info.jobName
+            const adv_roots:any[] = char_info.rows
+            for(const adv_root of adv_roots) {
+                const adv_name = adv_root.jobGrowName
+                yield { char_info, char_name, adv_root, adv_name }
+            }
+        }
+    }
+
+    *genAwks() {
+        const adv_info_gen = this.genAdvs()
+        for(const adv_info of adv_info_gen) {
+            let awk_node = adv_info.adv_root.next
+            while(awk_node != undefined) {
+                const awk_name = awk_node.jobGrowName
+                yield { ...adv_info, awk_node, awk_name }
+                awk_node = awk_node.next
+            }
+        }
+    }
+
+    countAdvNames() {
+        const generator = this.genAdvs()
+        const count:any = {}
+        for(const adv_info of generator) {
+            const { adv_name } = adv_info
+            console.log(adv_name)
+            count[adv_name] = adv_name in count ? count[adv_name] + 1 : 1
+        }
+        return count
+    }
+
+    countAwkNames() {
+        const generator = this.genAwks()
+        const count:any = {}
+        for(const awk_info of generator) {
+            const { awk_name } = awk_info
+            count[awk_name] = awk_name in count ? count[awk_name] + 1 : 1
+        }
+        return count
+    }
+
+    getRedundantAwks() {
+        const generator = this.genAwks()
+        const redundant_adv_names:any[] = []
+        for(const param of generator) {
+            const { awk_name } = param
+            if(this.count_awks[awk_name] > 1) {
+                redundant_adv_names.push(awk_name)
+            }
+        }
+    }
+
+    start() {
+        const file_path = "./data/df-jobs.json"
+        const json_content = JSON.parse(fs.readFileSync(file_path, "utf-8"))
+        this.char_infos = json_content.rows
+        this.count_advs = this.countAdvNames()
+        console.log(this.count_advs)
+        this.count_awks = this.countAwkNames()
+        console.log(this.count_awks)
+
+        const filterUniqueKeys = (count_map:any) => {
+            const filtered:any = {}
+            const entries = Object.entries(count_map)
+            for(const [key, count] of entries) {
+                if(count > 1) filtered[key] = count
+            }
+            return filtered
+        }
+
+        console.log(filterUniqueKeys(this.count_advs))
+        console.log(filterUniqueKeys(this.count_awks))
+    }
+}
+
+async function main() {
+    new Main().start()
+}
+main()
