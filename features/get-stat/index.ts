185b33b705eec01bd7d3cf3776e744fe198da4df
Vincentius P41siseus <vincentiusiseus@gmail.com>
2023-01-18 21:35:41 +0900

    Implement generator
    Worker 사용전 commit

--- features/get-stat/index.ts
+++ features/get-stat/index.ts
@@ -14,9 +14,22 @@ import { client } from "../../src/db"
 type Param = { job_id:string, job_grow_id:string }
 
 const PROMPT:boolean = false
+const METHODS = [
+    "getCharacterTimeline",
+    "getCharacterStatus",
+    "getCharacterEquipment",
+    "getCharacterAvatar",
+    "getCharacterCreature",
+    "getCharacterFlag",
+    "getCharacterTalisman",
+    "getCharacterSkillStyle",
+    "getCharacterSkillBuffEquipment",
+    "getCharacterSkillBuffAvatar",
+    "getCharacterSkillBuffCreature"
+]
 
 class Main {
-    params:Param[] = []
+    job_params:Param[] = []
     client:MongoClient
     db:Db
 
@@ -26,42 +39,53 @@ class Main {
         this.client = client
     }
 
-    async loadParams() {
+    async loadJobParams() {
         if(PROMPT) {
             const param = await new Prompt().start()
-            this.params.push(param)
+            this.job_params.push(param)
         }
         else {
             const gen = dfQueryableJobsGenerator()
             for(const param of gen) {
-                this.params.push({ job_id: param.jobId, job_grow_id: param.jobGrowId })
+                this.job_params.push({ job_id: param.jobId, job_grow_id: param.jobGrowId })
             }
         }
     }
 
-    async iterateAdventurers(cursor:FindCursor, param:Param) {
-        const adventurers = []
-        while(await cursor.hasNext()) {
-            const adventurer = await cursor.next()
-            adventurers.push(adventurer)
+    async *generator() {
+        for(const job_param of this.job_params) {
+            const query = {
+                jobId: job_param.job_id,
+                jobGrowId: job_param.job_grow_id,
+                level: 110
+            }
+            const cursor = this.db.collection("char-infos").find(query)
+
+            const adventurers = []
+            while(await cursor.hasNext()) {
+                const adventurer = await cursor.next()
+                adventurers.push(adventurer)
+
+                for(const dnf_api_method_name of METHODS) {
+                    yield { adventurer, dnf_api_method_name, job_param }
+                }
+            }
+            console.log(++this.debug_count, job_param, adventurers.length)
         }
-        console.log(++this.debug_count, param, adventurers.length)
     }
 
     async start() {
         await this.client.connect()
         this.db = this.client.db("dnf-data")
         
-        await this.loadParams()
+        await this.loadJobParams()
         
-        for(const param of this.params) {
-            const query = {
-                jobId: param.job_id,
-                jobGrowId: param.job_grow_id,
-                level: 110
-            }
-            const cursor = this.db.collection("char-infos").find(query)
-            await this.iterateAdventurers(cursor, param)
+        const gen = this.generator()
+        let count = 0
+        for await (const param of gen) {
+            console.log(param)
+            count++
+            if(count >= 100) break
         }
 
         await this.client.close()
