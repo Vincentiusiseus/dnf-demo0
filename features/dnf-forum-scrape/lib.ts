445ea399cb50200a723ad542800470fc048b6eed
Vincentius P41siseus <vincentiusiseus@gmail.com>
2023-01-27 11:36:43 +0900

    Add getLastPage for DF forum

--- features/dnf-forum-scrape/lib.ts
+++ features/dnf-forum-scrape/lib.ts
@@ -1,4 +1,25 @@
-import type { JSDOM } from "jsdom"
+// Node libs
+import * as crypto from "crypto"
+import * as https from "https"
+
+// NPM libs
+import _axios, { AxiosResponse } from "axios"
+import { JSDOM } from "jsdom"
+import iconv from "iconv-lite"
+
+const axios = _axios.create({
+    httpsAgent: new https.Agent({
+        keepAlive: true,
+        /**
+         * 2022-12-31 16:16
+         * Prevent `SSL routines:final_renegotiate:unsafe legacy renegotiation disabled`
+         * */
+        secureOptions: crypto.constants.SSL_OP_LEGACY_SERVER_CONNECT,
+    })
+})
+
+const BASE_URL = `https://df.nexon.com/df/community/dnfboard?mode=list&order=reg_date&order_type=DESC`
+
 
 export function scrapePostData(jsdom:JSDOM) {
     const document = jsdom.window.document
@@ -34,4 +55,65 @@ export function scrapePostData(jsdom:JSDOM) {
         output.push({ user_name, post_id, date, post_link })
     }
     return output
+}
+
+export async function loadPage(page:number):Promise<Buffer> {
+    const url = new URL("", BASE_URL)
+    url.searchParams.append("page", "" + page)
+    const response = await axios.get(url.href, { responseType: "arraybuffer" })
+    const res_data = response.data
+    return res_data 
+}
+
+export async function loadPageJsdom(page:number):Promise<any> {
+    const binary_html_content:Buffer = await loadPage(page)
+    const decoded_html_content = iconv.decode(binary_html_content, "euckr")
+    const jsdom = new JSDOM(decoded_html_content)
+    return jsdom
+}
+
+export async function getPageData(page:number) {
+    const jsdom:JSDOM = await loadPageJsdom(page)
+    const scraped_data = scrapePostData(jsdom)
+    return scraped_data
+}
+
+export async function _getLastPage(start_page:number=0):Promise<any> {
+    let inc_page_flag = true
+    let power = 0
+    let output:any = { start_page }
+    while(true) {
+        const page = start_page + 2 ** power
+        // console.log(`Make request for page ${page}`)
+        const data = await getPageData(page)
+        // console.log(page, power, data.length)
+
+        output.last_non_zero_page = output.page
+        output.last_non_zero_power = output.first_zero_power
+        output.page = page
+        output.first_zero_power = power
+
+        if(inc_page_flag) {
+            if(data.length == 0) {
+                inc_page_flag = false
+                break
+            }
+            else power++
+        }
+    }
+    return output
+}
+
+export async function getLastPage():Promise<number> {
+    let power = -1
+    let output:any = null
+    let count = 0
+    while(true) {
+        output = await _getLastPage(output == null ? 0 : output.last_non_zero_page)
+        count += output.first_zero_power + 1
+        // console.log(output)
+        if(output.first_zero_power == 0) break
+        power = output.last_non_zero_power
+    }
+    return output.page
 }
---
+++ features/dnf-forum-scrape/test/lib.ts
@@ -0,0 +1,6 @@
+import { getLastPage } from "../lib"
+
+async function main() {
+    console.log(await getLastPage())
+}
+main()
