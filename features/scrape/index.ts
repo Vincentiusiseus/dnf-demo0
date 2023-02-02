fb6b5d9eeae28ac9ce7de58ddf522d200d248b90
Vincentius P41siseus <vincentiusiseus@gmail.com>
2023-02-03 07:29:30 +0900

    Add more prompt

--- features/scrape/index.ts
+++ features/scrape/index.ts
@@ -3,7 +3,27 @@ import { prompt } from "./prompt"
 class Main {
     async start() {
         const result = await prompt()
-        console.log(result)
+        const target = result.target
+        const skip_page_count = result.skip_page_count
+
+        const is_all = target.length == 0 || target.includes("all")
+
+        console.log({ result, is_all, skip_page_count })
+
+        if(is_all || target.includes("df-board")) {
+            /**
+             * 2023-02-03 07:03
+             * Once scraped, only new posts need to be scraped
+             */
+        }
+        
+        if(is_all || target.includes("dunfaoff")) {
+
+        }
+
+        if(is_all || target.includes("dundam")) {
+
+        }
     }
 }
 
--- features/scrape/prompt.ts
+++ features/scrape/prompt.ts
@@ -1,20 +1,34 @@
 import inquirer from "inquirer"
 
 type PromptResult = {
-    target: "df-board" | "dunfaoff" | "dundam" | "all"
+    target: Array<"df-board" | "dunfaoff" | "dundam" | "all">
+    skip_page_count: boolean
 }
 
 export async function prompt() {
-    const result = await inquirer.prompt([
+    const result:PromptResult = await inquirer.prompt([
         {
             name: "target",
+            message: "스크레이핑 대상 (기본: all)",
             type: "checkbox",
             choices: [
                 { name: "던파 게시판", value: "df-board" },
                 { name: "던파오프", value: "dunfaoff" },
                 { name: "던담", value: "dundam" },
                 "all"
-            ]
+            ],
+            /**
+             * 2023-02-03 07:26
+             * `default`가 체크되서 보여짐. 원치 않는 옵션이라면 유저가 체크해제 해야함.
+             * 차라리 prompt 끝나고 기본값 매기는게 나을듯
+             */
+            // default: ["all"]
+        },
+        {
+            name: "skip_page_count",
+            message: "총 페이지 세기 스킵? (기본: 스킵 안함)",
+            type: "confirm",
+            default: false
         }
     ])
     return result
