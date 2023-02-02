9c8ff62d794264cc1dd89c9147fbedf658f3f0c8
Vincentius P41siseus <vincentiusiseus@gmail.com>
2023-02-03 05:46:58 +0900

    Update README

--- features/main/index.ts
+++ features/main/index.ts
@@ -4,10 +4,24 @@ import { describe } from "yargs"
 // My libs
 import { prompt, PromptResult } from "./prompt"
 
+import {} from "../get-stat"
+import {} from "../get-last-page"
+
 class Main {
     prompt_result:PromptResult
     async start() {
         this.prompt_result = await prompt()
+
+        const task = this.prompt_result.task
+        if(task == "stat") {
+
+        }
+        else if(task == "total") {
+
+        }
+        else if(task == "ranking") {
+
+        }
     }
 }
 
